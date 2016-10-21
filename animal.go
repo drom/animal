c556a65b435982246c4ebd2a7ec2809762f624bc
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2016-10-21 16:04:15 +0100

    implemented askQuestion and guessAnimal. The game now works but won't learn new animals / questions yet.

--- animal.go
+++ animal.go
@@ -28,8 +28,8 @@ var animals = []string{"Fish", "Bird"}
 var questions = []Question{
 	Question{
 		Question:  "Does it swim",
-		YesTarget: Target{Type: "", Index: 0},
-		NoTarget:  Target{Type: "", Index: 1},
+		YesTarget: Target{Type: "a", Index: 0},
+		NoTarget:  Target{Type: "a", Index: 1},
 	},
 }
 
@@ -54,6 +54,7 @@ func main() {
 			//output the animal list and then ask again
 			listKnownAnimals()
 		case "y": //Yes
+			askQuestion(0)
 		default:
 			fmt.Println("I don't recognise that command")
 		}
@@ -66,10 +67,55 @@ func playIntroMessage() {
 }
 
 func listKnownAnimals() {
-	fmt.Println("I know the following animals")
+	fmt.Println("Animals I already know are:")
 	for _, v := range animals {
 		fmt.Println(v)
 	}
+}
+
+func askQuestion(i int) {
+	var t *Target
+
+	q := questions[i]
+
+	for t == nil {
+		fmt.Println(q.Question + "?")
+
+		switch strings.ToLower(getInput())[:1] {
+		case "n": //No
+			t = &q.NoTarget
+		case "y": //Yes
+			t = &q.YesTarget
+		default:
+			fmt.Println("Please answer yes or no.")
+		}
+	}
+
+	//ask a question or guess an animal
+	if t.Type == "a" {
+		guessAnimal(t.Index)
+	} else {
+		askQuestion(t.Index)
+	}
+}
+
+func guessAnimal(i int) bool {
+	for true {
+		fmt.Println("Is " + animals[i] + " your animal?")
+
+		switch strings.ToLower(getInput())[:1] {
+		case "n": //No
+			return false
+		case "y": //Yes
+			return true
+		default:
+			fmt.Println("Please answer yes or no.")
+		}
+	}
+
+	//this is impossible to reach, as the loop is infinite unless return is triggered by a case
+	//but golint wants a return statement
+	return false
 }
 
 func getInput() (input string) {
