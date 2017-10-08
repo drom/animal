bc5afba7b5382896ffd6078bf98047e5bc819d81
sdeseille <sebastien.deseille@gmail.com>
2017-10-08 22:41:20 +0200

    add somme functions and loop
    one step beyond...Add main loop and some messages functions

--- perl/animal.pl
+++ perl/animal.pl
@@ -3,5 +3,34 @@ use warnings;
 use Data::Dumper;
 
 my $data = require('data.pm');
+my $is_playing = 0;
+#print Dumper $data;
 
-print Dumper $data;
+
+
+sub start_msg {
+    print $data->{'start'},"\n";
+    $is_playing = 1;
+}
+
+sub exit_msg {
+    print $data->{'exit'},"\n";
+}
+
+sub game_loop {
+    print "In game...\n";
+    while($is_playing){
+        my $line = <STDIN>;
+        chomp $line;
+        if ("$line" eq 'Q'){
+            $is_playing = 0;
+        }
+        else{
+            print "$line\n";
+        }
+    }
+}
+
+start_msg();
+game_loop();
+exit_msg();
